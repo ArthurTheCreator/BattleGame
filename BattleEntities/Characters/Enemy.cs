bc3b8ffdd78037f77cb09f4a016d7c50f3735121
Arthur Bomfim <arthurbao10@gmail.com>
2025-03-25 13:13:59 -0300

    add - HealthPotionStock

--- BattleEntities/Characters/Enemy.cs
+++ BattleEntities/Characters/Enemy.cs
@@ -6,7 +6,7 @@ namespace BattleEntities.Characters
     {
         public string Name { get; set; }
         public int Hp { get; set; } = 100;
-        public List<HealthPotion> ListHealthPotion { get; set; } = new List<HealthPotion>() 
+        public List<HealthPotion> HealthPotionStock { get; set; } = new List<HealthPotion>() 
         { 
             new HealthPotion(),
             new HealthPotion()
