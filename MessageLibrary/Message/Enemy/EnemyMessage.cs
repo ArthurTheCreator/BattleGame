f9c7f55f1f51f5825039d513927ea15719c69f2e
José Henrique <josehenriquefernandes346@gmail.com>
2025-04-09 10:20:55 -0300

    Add regions

--- MessageLibrary/Message/Enemy/EnemyMessage.cs
+++ MessageLibrary/Message/Enemy/EnemyMessage.cs
@@ -236,6 +236,8 @@ public static class EnemyMessage
 
     #endregion
 
+    #region Defense
+
     public static Dictionary<EnumLanguage, List<string>> BothDefendedMessages = new()
     {
         { EnumLanguage.Portuguese, new()
@@ -298,5 +300,5 @@ public static class EnemyMessage
         }
     };
 
-
+    #endregion
 }
--- MessageLibrary/Message/GetMessage/GetEnemyMessage.cs
+++ MessageLibrary/Message/GetMessage/GetEnemyMessage.cs
@@ -12,6 +12,10 @@ public static class GetEnemyMessage
 
     public static EnumLanguage EnumLanguage { get; set; }
 
+    #region Potion
+
+    #region Success
+
     public static string GetEnemySuccesTakePotionMessage
     {
         get
@@ -20,6 +24,10 @@ public static class GetEnemyMessage
             return messages[GetNext(messages.Count)];
         }
     }
+
+    #endregion
+
+    #region Failure
 
     public static string GetEnemyFailureTakePotionStockMessage
     {
@@ -30,11 +38,23 @@ public static class GetEnemyMessage
         }
     }
 
+    #endregion
+
+    #endregion
+
+    #region Attack
+
+    #region Light
+
     public static string GetEnemySuccessLightAttackMessages(int damage)
     {
         var messages = EnemyMessage.EnemyLightAttackMessages[EnumLanguage];
         return messages[GetNext(messages.Count)].Replace("{damage}", damage.ToString());
     }
+
+    #endregion
+
+    #region Medium
 
     public static string GetEnemySuccessMediumAttackMessages(int damage)
     {
@@ -42,11 +62,18 @@ public static class GetEnemyMessage
         return messages[GetNext(messages.Count)].Replace("{damage}", damage.ToString());
     }
 
+    #endregion
+
+    #region Heavy
     public static string GetEnemySuccessHeavyAttackMessages(int damage)
     {
         var messages = EnemyMessage.EnemyHeavyAttackMessages[EnumLanguage];
         return messages[GetNext(messages.Count)].Replace("{damage}", damage.ToString());
     }
+
+    #endregion
+
+    #region Missed
 
     public static string GetEnemyMissedButPlayerDefendedMessages(string playerName)
     {
@@ -63,6 +90,11 @@ public static class GetEnemyMessage
         }
     }
 
+    #endregion
+
+    #endregion
+
+    #region Defense
     public static string GetEnemyUsingDefense
     {
         get
@@ -80,6 +112,8 @@ public static class GetEnemyMessage
             return messages[GetNext(messages.Count)];
         }
     }
+
+    #endregion
 
     private static int GetNext(int length)
     {
